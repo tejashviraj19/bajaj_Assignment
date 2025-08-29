e7f9a719c6bb792511e7bbc809a462900a16dafd
Tejashvi <tejashviraj2022@vitbhopal.ac.in>
2025-08-29 11:28:08 +0530

    created

---
+++ api/bfhl.js
@@ -0,0 +1,80 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+const FULL_NAME = "john_doe";  
+const DOB = "17091999";       
+const EMAIL = "john@xyz.com";  
+const ROLL_NUMBER = "ABCD123"; 
+
+
+function alternateCapsReverse(str) {
+    let reversed = str.split('').reverse();
+    return reversed.map((ch, i) => i % 2 === 0 ? ch.toUpperCase() : ch.toLowerCase()).join('');
+}
+
+
+app.get('/bfhl', (req, res) => {
+    return res.status(200).json({
+        operation_code: 1
+    });
+});
+
+app.post('/bfhl', (req, res) => {
+    try {
+        const data = req.body.data || [];
+
+        let evenNumbers = [];
+        let oddNumbers = [];
+        let alphabets = [];
+        let specialChars = [];
+        let sum = 0;
+        let allAlphabets = '';
+
+        data.forEach(item => {
+            if (/^\d+$/.test(item)) { // Check if it's a number
+                let num = parseInt(item, 10);
+                sum += num;
+                if (num % 2 === 0) {
+                    evenNumbers.push(item);
+                } else {
+                    oddNumbers.push(item);
+                }
+            } else if (/^[a-zA-Z]+$/.test(item)) { // Alphabets
+                alphabets.push(item.toUpperCase());
+                allAlphabets += item;
+            } else { // Special characters
+                specialChars.push(item);
+            }
+        });
+
+        const concatString = alternateCapsReverse(allAlphabets);
+
+        return res.status(200).json({
+            is_success: true,
+            user_id: `${FULL_NAME}_${DOB}`,
+            email: EMAIL,
+            roll_number: ROLL_NUMBER,
+            odd_numbers: oddNumbers,
+            even_numbers: evenNumbers,
+            alphabets: alphabets,
+            special_characters: specialChars,
+            sum: sum.toString(),
+            concat_string: concatString
+        });
+
+    } catch (error) {
+        return res.status(500).json({
+            is_success: false,
+            message: error.message
+        });
+    }
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+
+module.exports = app;
